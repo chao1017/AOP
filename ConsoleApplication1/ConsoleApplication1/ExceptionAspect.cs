2bcd9ab277eb98f058b637cdcbc0e4b1d79857c4
chao1017 <chaoshuangjui@gmail.com>
2015-03-26 13:26:10 +0800

    新增EException Handle 範例

---
+++ ConsoleApplication1/ConsoleApplication1/ExceptionAspect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+using PostSharp.Aspects;
+
+namespace ConsoleApplication1
+{
+    [Serializable]
+    public class ExceptionAspect : OnExceptionAspect
+    {
+        public string Message { get; set; }
+
+        public Type ExceptionType { get; set; }
+
+        public FlowBehavior Behavior { get; set; }
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            string msg = DateTime.Now + ": " + Message + Environment.NewLine;
+            msg += string.Format("{0}: Error running {1}. {2}{3}{4}", DateTime.Now, args.Method.Name, args.Exception.Message, Environment.NewLine, args.Exception.StackTrace);
+            Debug.WriteLine(msg);
+            args.FlowBehavior = FlowBehavior.Continue;
+        }
+
+        public override Type GetExceptionType(System.Reflection.MethodBase targetMethod)
+        {
+            return ExceptionType;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+using PostSharp.Aspects;
+
+namespace ConsoleApplication1
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            ThrowSampleExecption();
+            
+            Console.ReadLine();
+        }
+
+        [ExceptionAspect(ExceptionType = typeof(ApplicationException), Message = "An example exception.", Behavior=FlowBehavior.RethrowException)]
+        private static void ThrowSampleExecption()
+        {
+            throw new ApplicationException("Sample Exception For AOP");
+
+            //FlowBehavior behavior = FlowBehavior.RethrowException;
+        }
+    }
+}
